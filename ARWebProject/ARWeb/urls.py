ec27cf9ca136bb72e6ff20d51d6e2f49d15c69cf
tahab <bezcitaha@gmail.com>
2024-10-29 14:45:22 +0300

    login ekrani eklendi

--- ARWebProject/ARWeb/urls.py
+++ ARWebProject/ARWeb/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 app_name="ARWeb"
 
 urlpatterns = [
-    path('home/',views.index, name='home')
+    path('home/',views.index, name='home'),
+    path('login/', views.login_view, name='login'),
 ]
--- ARWebProject/ARWeb/views.py
+++ ARWebProject/ARWeb/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'../templates/ARWebProject/index.html')
+
+def login_view(request):
+    return render(request, '../templates/ARWebProject/login.html')
